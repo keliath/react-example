9c28bd8ece3f25b9ca19b0b87ca2fd0cce121ac9
carlos altamirano <carlos0altamirano@gmail.com>
2021-08-26 20:07:13 -0500

    add component controlled

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,10 +1,23 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 function NewExpense(props) {
+  const [filter, setFilter] = useState("2020");
+
+  const saveExpenseFilterHandler = (filterData) => {
+    // console.log(filterData);
+    setFilter(filterData);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filter}
+        onSaveExpenseFilter={saveExpenseFilterHandler}
+      />
       <ExpenseItem
         title="hardcoded"
         amount={props.items[0].amount}
